2f623858c2fd31b7c31fa8dbf7929b87a871ccab
Yash Duhan <yash2004duhan@gmail.com>
2025-03-01 18:38:03 +0530

    added route for ec2

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -3,3 +3,6 @@
 
 // Production Route
 export const API_BASE_URL = "https://veritrust-backend.vercel.app"
+
+// Ec2 Route
+// export const API_BASE_URL = "http://ec2-13-232-166-200.ap-south-1.compute.amazonaws.com"
